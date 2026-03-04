650f2ddb858a4fbfba49f57736136177b444669c
shivanshugupta456 <rajkishoregupta716@gmail.com>
2026-03-04 16:35:55 +0530

    Update login API endpoint to '/user/login'

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("https://bookstoreapp-a1dp.onrender.com/login", userInfo)
+      .post("https://bookstoreapp-a1dp.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
