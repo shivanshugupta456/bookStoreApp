da4c40ae6693c08332134d058eedfb68cfb5194e
shivanshugupta456 <rajkishoregupta716@gmail.com>
2026-03-04 18:24:29 +0530

    gr

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://bookstoreapp-a1dp.onrender.com/book");
+        const res = await axios.get("https://bookstoreapp-backend-k17k.onrender.com/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
