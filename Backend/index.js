81b75d224d0b6367f54c25ca81d50b2f1cad7a58
shivanshugupta456 <rajkishoregupta716@gmail.com>
2026-03-04 18:59:16 +0530

    Refactor MongoDB connection to use promises

--- Backend/index.js
+++ Backend/index.js
@@ -17,15 +17,13 @@ const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
 // connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+mongoose.connect(URI)
+    .then(() => {
     console.log("Connected to mongoDB");
-} catch (error) {
+})
+    .catch ((error)=> {
     console.log("Error: ", error);
-}
+});
 
 // defining routes
 app.use("/book", bookRoute);
